c5fc1000bb5019c80499e4b4892c7645c2bb5861
cetmoko <gayatrimm17@gmail.com>
2025-01-12 12:12:10 +0530

    completed exercises 01-05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,20 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    if (((Number.isInteger(x)) && (x > 0)) && ((Number.isInteger(y)) && (y > 0))) {
+        let sum = 0;
+        if (x < y) {
+            for (let i = x; i <= y; i++) {
+                sum += i;
+            }
+        } else {
+            for (let i = y; i <= x; i++) {
+                sum += i;
+            }
+        }
+        return sum;
+    } else {
+        return "ERROR";
+    }
+    
 
 };
 
